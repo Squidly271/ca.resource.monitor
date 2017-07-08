c34e51e79bfb17b7e7a2d7c8df8f0204ceb12904
Squidly271 <azawadzki@arrowgames.com>
2017-07-08 12:26:50 -0400

    Initial test release

---
+++ copy_to_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p "/tmp/GitHub/ca.resource.monitor/source/ca.resource.monitor/usr/local/emhttp/plugins/ca.resource.monitor/"
+
+cp /usr/local/emhttp/plugins/ca.resource.monitor/* /tmp/GitHub/ca.resource.monitor/source/ca.resource.monitor/usr/local/emhttp/plugins/ca.resource.monitor -R -v -p
+
+
---
+++ pkg_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd source/ca.resource.monitor
+pkg_build.sh
+
